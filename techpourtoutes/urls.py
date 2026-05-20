a2da7212910257f576f943e54cdd3f6b321b8bad
Quentin Blanc <46218316+qblanc@users.noreply.github.com>
2026-05-20 17:03:43 +0200

    add legal views (#37)

--- techpourtoutes/urls.py
+++ techpourtoutes/urls.py
@@ -3,7 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Coallition
     path("", views.coallition_index, name="coallition_index"),
     path("je-deviens-mentor/", views.mentor_landing, name="mentor_landing"),
     path("bienvenue-dans-la-coallition/", views.mentor_success, name="mentor_success"),
+    # Legals
+    path("donnees-personnelles/", views.donnees_personnelles, name="donnees_personnelles"),
+    path("conditions-generales/", views.conditions_generales, name="conditions_generales"),
+    path("mentions-legales/", views.mentions_legales, name="mentions_legales"),
+    path("accessibilite/", views.accessibilite, name="accessibilite"),
+    path("schema-pluriannuel-accessibilite", views.schema_pluriannuel, name="schema_pluriannuel"),
 ]
--- techpourtoutes/views/__init__.py
+++ techpourtoutes/views/__init__.py
@@ -1,7 +1,19 @@
 from .coallition_views import coallition_index, mentor_landing, mentor_success
+from .legal_views import (
+    accessibilite,
+    conditions_generales,
+    donnees_personnelles,
+    mentions_legales,
+    schema_pluriannuel,
+)
 
 __all__ = [
     "coallition_index",
     "mentor_landing",
     "mentor_success",
+    "accessibilite",
+    "schema_pluriannuel",
+    "donnees_personnelles",
+    "conditions_generales",
+    "mentions_legales",
 ]
