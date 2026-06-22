4e846b1d783f361461a0d5dbaf8fc85134cca824
Quentin Blanc <46218316+qblanc@users.noreply.github.com>
2026-06-22 17:32:38 +0200

    fix brevo sync

--- techpourtoutes/services/brevo_api/delete_contact.py
+++ techpourtoutes/services/brevo_api/delete_contact.py
@@ -3,7 +3,7 @@ from .base_service import BrevoApiBaseService
 
 class DeleteBrevoContact(BrevoApiBaseService):
     def perform(self, *, ext_id: str, list_id: int) -> None:
-        self.request(method="get_contact", ext_id=ext_id)
+        self.request(method="get_contact", identifier=ext_id)
         if len(self.brevo_response_body.list_ids) > 1:
             self.request(method="remove_contact_from_list", ext_id=ext_id, list_id=list_id)
         else:
--- techpourtoutes/services/brevo_api/upsert_contact.py
+++ techpourtoutes/services/brevo_api/upsert_contact.py
@@ -1,16 +1,8 @@
 from .base_service import BrevoApiBaseService
-from .mappings import brevo_attributes_for, brevo_list_id_for
 
 
 class UpsertBrevoContact(BrevoApiBaseService):
-    def perform(self, *, instance) -> None:
-        list_id = brevo_list_id_for(instance)
-        attributes = brevo_attributes_for(instance)
-        if list_id is None or attributes is None:
-            return
+    def perform(self, *, ext_id: str | None = None, list_id: int, attributes: dict) -> None:
         self.request(
-            method="upsert_contact",
-            ext_id=str(instance.pk),
-            list_id=list_id,
-            attributes=attributes,
+            method="upsert_contact", ext_id=ext_id, list_id=list_id, attributes=attributes
         )
