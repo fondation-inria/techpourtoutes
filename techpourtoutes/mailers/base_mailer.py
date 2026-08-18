7b6880002adbc32f82cb28f2f9b1e5c9e1ef85ff
Sonia <sonia.hassuan@fondation-inria.fr>
2026-08-18 16:53:13 +0200

    rename logo

--- techpourtoutes/mailers/base_mailer.py
+++ techpourtoutes/mailers/base_mailer.py
@@ -50,6 +50,6 @@ class BaseMailer:
     def _base_context():
         return {
             "logo_url": settings.SITE_URL
-            + staticfiles_storage.url("images/techpourtoutes-logo-new.svg"),
+            + staticfiles_storage.url("images/techpourtoutes-logo-new-beneficiary.svg"),
             "base_url": settings.SITE_URL,
         }
